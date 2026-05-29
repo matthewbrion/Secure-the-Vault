700426b8b17c1023a9201e42327ac4237383bfc2
Matt <matthewbrion@me.com>
2026-05-28 21:54:13 -0400

    Secure the Vault workshop

---
+++ index.js
@@ -0,0 +1,19 @@
+let code1 = 30
+console.log(code1)
+code1 = 7 + 3
+console.log(code1)
+code2 = 180 / 2 - 50
+console.log(code2)
+const code3 = 17 % 9 + 31
+console.log(code3)
+
+const message = "The vault has been secured.  The combination is: "
+console.log(message)
+
+var codeA = '"' + code1 + "-" + code2 + "-" + code3 + '"'
+console.log(codeA)
+
+var codeB = `"${code1}-${code2}-${code3}"`
+console.log(codeB)
+
+console.log(message, codeA, codeB)
